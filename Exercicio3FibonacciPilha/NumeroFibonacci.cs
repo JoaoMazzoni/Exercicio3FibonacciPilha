308211f31bd10a3f4ce770fc4750617bb85e88cd
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-05-02 20:16:43 -0300

    Add project files.

---
+++ Exercicio3FibonacciPilha/NumeroFibonacci.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Exercicio3FibonacciPilha
+{
+    internal class NumeroFibonacci
+    {
+        int numero;
+        NumeroFibonacci anterior;
+
+        public NumeroFibonacci(int numero)
+        {
+            this.numero = numero;
+            anterior = null;
+        }
+
+        public void setAnterior(NumeroFibonacci numero)
+        {
+            anterior = numero;
+        }
+
+        public NumeroFibonacci getAnterior()
+        {
+            return anterior;
+        }
+
+
+        public int getValor() //Função para pegar o valor do numero.
+        {
+            return this.numero;
+        }
+
+
+
+    }
+}
---
+++ Exercicio3FibonacciPilha/PilhaFibonacci.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace Exercicio3FibonacciPilha
+{
+
+    internal class PilhaFibonacci
+    {
+        NumeroFibonacci headNumero;
+
+
+        public PilhaFibonacci()
+        {
+            headNumero = null;
+            Console.WriteLine("\nPilha Criada!\n");
+        }
+        bool Empty()
+        {
+            return headNumero == null;
+        }
+
+        public void push(NumeroFibonacci auxNumero)
+        {
+            if (Empty() == true)
+            {
+                headNumero = auxNumero;
+            }
+            else
+            {
+                auxNumero.setAnterior(headNumero);
+                headNumero = auxNumero;
+            }
+            
+        }
+
+        public void imprimirPilha()
+        {
+            Console.WriteLine("\nValores na pilha\n");
+            NumeroFibonacci numeroAtual = headNumero;
+            while (numeroAtual != null)
+            {
+                Console.WriteLine(numeroAtual.getValor());
+                numeroAtual = numeroAtual.getAnterior();
+            }
+        }
+
+
+
+    }        
+
+}
